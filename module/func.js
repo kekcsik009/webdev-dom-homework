b2adbe6613f88b52345628505aaae51d82ef9a46
Даниил <chirun1999@mail.ru>
2023-09-21 23:17:13 +0700

    s

--- module/func.js
+++ module/func.js
@@ -1,15 +1,15 @@
 
 
-export const addDate = () =>{
-    const date = new Date();
-    let time = {
-        hour: 'numeric',
-        minute: 'numeric'
-    };
-    let year = {
-        year: '2-digit',
-        month: 'numeric',
-        day: 'numeric'
-    }// Выводим дату в нужной для нас форме и русской локализацией
-    return  date.toLocaleString("ru", year) + " " + date.toLocaleString('ru', time);
-}
+// export const addDate = () =>{
+//     const date = new Date();
+//     let time = {
+//         hour: 'numeric',
+//         minute: 'numeric'
+//     };
+//     let year = {
+//         year: '2-digit',
+//         month: 'numeric',
+//         day: 'numeric'
+//     }// Выводим дату в нужной для нас форме и русской локализацией
+//     return  date.toLocaleString("ru", year) + " " + date.toLocaleString('ru', time);
+// }
